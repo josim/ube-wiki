da9d9abf3a2af0035da26891664693f9f210af60
joe simon <johannessimon@outlook.de>
2026-03-23 16:22:14 +0100

    Contract Update

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,8 +7,8 @@ export const NETWORK_TYPE = 'shadownet'
 // Deployed contracts (Shadownet)
 export const MULTISIG_CONTRACT = 'KT1KeGd4YtjcKqgyiXUPJQkm2iYA3fQwLGQP'
 export const MODERATOR_CONTRACT = 'KT1MTj52FxbP2C5pUCAZqx5adwbNJ4JeYz48'
-export const REGISTRY_CONTRACT = 'KT1T3V92u1fJwrLL9NBsspcMgnUKkhXYXbxm'
-export const PROPOSAL_CONTRACT = 'KT1DS7NZVg8VpHk2XpnEYGfWD6NefapdHzCa'
+export const REGISTRY_CONTRACT = 'KT1SGNoFHg8QhQqXcnomzsdEZx1uGFtJX1Pr'
+export const PROPOSAL_CONTRACT = 'KT1CxkcdfQBLgbQum5D9oJ2GdPCTZHfhVzVK'
 
 // Teia token (FA2)
 export const TEIA_TOKEN_CONTRACT = 'KT1Mi8MejYS9agBUnhuHGHSvLf6ZVuwMgsM3'
